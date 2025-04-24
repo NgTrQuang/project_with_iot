8a562d78f13963eea74a7d8973f987d77199c1d2
ntquang@it <nguyentriquang23@gmail.com>
2025-04-24 07:35:03 +0700

    updated SERVER

--- SERVER/src/mqtt/mqttClient.js
+++ SERVER/src/mqtt/mqttClient.js
@@ -2,17 +2,17 @@ const mqtt = require("mqtt");
 
 // Cấu hình kết nối HiveMQ Cloud
 const options = {
-  port: 1883,     // 8883
-  host: "192.168.2.12", // 192.168.2.12 ip của máy chủ server cần cấu hình mosquitto   // "53052f65a6404b3b90d30c8b2fdcf5b1.s1.eu.hivemq.cloud"
-  protocol: "mqtt", // dùng mqtts khi cổng 8883 (TLS)
-  // username: "hivemq.webclient.1745053412141",
-  // password: "WR.aZBu?6tD0%4qk*5Ig",
+  port: 8883,     // 8883
+  host: "53052f65a6404b3b90d30c8b2fdcf5b1.s1.eu.hivemq.cloud", // 192.168.2.12 ip của máy chủ server cần cấu hình mosquitto   // "53052f65a6404b3b90d30c8b2fdcf5b1.s1.eu.hivemq.cloud"
+  protocol: "mqtts", // dùng mqtts khi cổng 8883 (TLS)
+  username: "hivemq.webclient.1745053412141",
+  password: "WR.aZBu?6tD0%4qk*5Ig",
 };
 
 const client = mqtt.connect(options);
 
 client.on("connect", () => {
-  console.log("✅ Backend kết nối MQTT MOSQUITTO thành công");    // HiveMQ Cloud
+  console.log("✅ Backend kết nối MQTT HiveMQ Cloud thành công");    // HiveMQ Cloud
 });
 
 client.on("error", (err) => {
